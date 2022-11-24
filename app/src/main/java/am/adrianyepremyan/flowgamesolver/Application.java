2497dae2d21db9ff93e87252220cc8a199eae6b2
Yepremyan <adrian.yepremyan@picsart.com>
2022-11-24 17:20:06 +0400

    made the application runnable with gradlew run. now sorting the initial flows as part of the algorithm, to first join the flows closest to each other (not sure if this is correct). added several new maps that were solved immediately!

--- app/src/main/java/am/adrianyepremyan/flowgamesolver/Application.java
+++ app/src/main/java/am/adrianyepremyan/flowgamesolver/Application.java
@@ -8,13 +8,9 @@ import am.adrianyepremyan.flowgamesolver.solver.Solver;
 public class Application {
 
     public static void main(String[] args) {
-        final var map = new GameMap(5, 5);
-        map.addInitialFlows("Red", new Point(0, 0), new Point(1, 4));
-        map.addInitialFlows("Green", new Point(2, 0), new Point(1, 3));
-        map.addInitialFlows("Blue", new Point(2, 1), new Point(2, 4));
-        map.addInitialFlows("Yellow", new Point(4, 0), new Point(3, 3));
-        map.addInitialFlows("Orange", new Point(4, 1), new Point(3, 4));
+        final var map = mapExample8x8();
 
+        System.out.println("Game map is:");
         final var mapPrinter = new DefaultGameMapPrinter();
         map.print(mapPrinter);
 
@@ -23,5 +19,55 @@ public class Application {
 
         System.out.println("Solution:");
         map.print(mapPrinter);
+    }
+
+    private static GameMap mapExample5x5() {
+        final var map = new GameMap(5, 5);
+        map.addInitialFlows("Red", new Point(0, 0), new Point(1, 4));
+        map.addInitialFlows("Green", new Point(2, 0), new Point(1, 3));
+        map.addInitialFlows("Blue", new Point(2, 1), new Point(2, 4));
+        map.addInitialFlows("Yellow", new Point(4, 0), new Point(3, 3));
+        map.addInitialFlows("Orange", new Point(4, 1), new Point(3, 4));
+        return map;
+    }
+
+    private static GameMap mapExample7x7() {
+        final var map = new GameMap(7, 7);
+        map.addInitialFlows("Blue", new Point(0, 1), new Point(1, 2));
+        map.addInitialFlows("Yellow", new Point(1, 1), new Point(2, 3));
+        map.addInitialFlows("Orange", new Point(0, 2), new Point(3, 5));
+        map.addInitialFlows("Red", new Point(0, 6), new Point(6, 2));
+        map.addInitialFlows("Green", new Point(6, 1), new Point(5, 5));
+        map.addInitialFlows("Cyan", new Point(5, 1), new Point(5, 4));
+        return map;
+    }
+
+    private static GameMap mapExample8x8() {
+        final var map = new GameMap(8, 8);
+        map.addInitialFlows("Kanach", new Point(6, 0), new Point(3, 5));
+        map.addInitialFlows("Indigo", new Point(2, 0), new Point(7, 4));
+        map.addInitialFlows("Yellow", new Point(6, 1), new Point(4, 3));
+        map.addInitialFlows("Orange", new Point(3, 0), new Point(5, 0));
+        map.addInitialFlows("Cyan", new Point(2, 1), new Point(5, 1));
+        map.addInitialFlows("Red", new Point(2, 2), new Point(2, 5));
+        map.addInitialFlows("Pink", new Point(1, 1), new Point(6, 4));
+        map.addInitialFlows("Blue", new Point(3, 2), new Point(4, 4));
+        return map;
+    }
+
+    private static GameMap mapExample10x10() {
+        final var map = new GameMap(10, 10);
+        map.addInitialFlows("Kanach", new Point(0, 0), new Point(9, 0));
+        map.addInitialFlows("Indigo", new Point(0, 2), new Point(7, 8));
+        map.addInitialFlows("Fioletovi", new Point(0, 4), new Point(3, 2));
+        map.addInitialFlows("Yellow", new Point(0, 5), new Point(5, 2));
+        map.addInitialFlows("Orange", new Point(4, 4), new Point(5, 3));
+        map.addInitialFlows("Cyan", new Point(2, 3), new Point(1, 4));
+        map.addInitialFlows("Gray", new Point(0, 6), new Point(4, 6));
+        map.addInitialFlows("Red", new Point(1, 6), new Point(3, 6));
+        map.addInitialFlows("White", new Point(1, 7), new Point(3, 7));
+        map.addInitialFlows("Pink", new Point(0, 9), new Point(7, 3));
+        map.addInitialFlows("Blue", new Point(4, 5), new Point(9, 9));
+        return map;
     }
 }
--- app/src/main/java/am/adrianyepremyan/flowgamesolver/solver/Solver.java
+++ app/src/main/java/am/adrianyepremyan/flowgamesolver/solver/Solver.java
@@ -1,5 +1,10 @@
 package am.adrianyepremyan.flowgamesolver.solver;
 
+import static am.adrianyepremyan.flowgamesolver.map.domain.FlowDirection.DOWN;
+import static am.adrianyepremyan.flowgamesolver.map.domain.FlowDirection.LEFT;
+import static am.adrianyepremyan.flowgamesolver.map.domain.FlowDirection.RIGHT;
+import static am.adrianyepremyan.flowgamesolver.map.domain.FlowDirection.UP;
+
 import am.adrianyepremyan.flowgamesolver.helper.Point;
 import am.adrianyepremyan.flowgamesolver.map.GameMap;
 import am.adrianyepremyan.flowgamesolver.map.domain.Flow;
@@ -15,7 +20,18 @@ public class Solver {
 
     public void solve(GameMap map) {
         final var matrix = map.getMatrix();
-        final var initialFlow = map.getInitialFlowList().get(0);
+        final var initialFlows = map.getInitialFlowList();
+        initialFlows.sort((if1, if2) -> {
+            int if1Steps = Math.abs(if1.p1().x() - if1.p2().x()) + Math.abs(if1.p1().y() - if1.p2().y());
+            int if2Steps = Math.abs(if2.p1().x() - if2.p2().x()) + Math.abs(if2.p1().y() - if2.p2().y());
+            if (if1Steps > if2Steps) {
+                return 1;
+            } else if (if1Steps < if2Steps) {
+                return -1;
+            }
+            return 0;
+        });
+        final var initialFlow = initialFlows.get(0);
 
         if (!solveRecursively(map, matrix[initialFlow.p1().y()][initialFlow.p1().x()], map.getInitialFlowList(), 0)) {
             throw new RuntimeException("Game has no solution!");
@@ -25,17 +41,13 @@ public class Solver {
     private boolean solveRecursively(GameMap map, Flow flow,
                                      List<InitialFlow> initialFlowList,
                                      int initialFlowIndex) {
-        System.out.println("Solving recursively for flow: " + flow);
-        printer.print(map);
-
         final int startX = flow.getPoint().x();
         final int startY = flow.getPoint().y();
 
-        return solveWithDirection(map, flow, initialFlowList, initialFlowIndex, startX, startY - 1, FlowDirection.UP)
-            || solveWithDirection(map, flow, initialFlowList, initialFlowIndex, startX, startY + 1, FlowDirection.DOWN)
-            || solveWithDirection(map, flow, initialFlowList, initialFlowIndex, startX - 1, startY, FlowDirection.LEFT)
-            ||
-            solveWithDirection(map, flow, initialFlowList, initialFlowIndex, startX + 1, startY, FlowDirection.RIGHT);
+        return solveWithDirection(map, flow, initialFlowList, initialFlowIndex, startX, startY - 1, UP)
+            || solveWithDirection(map, flow, initialFlowList, initialFlowIndex, startX, startY + 1, DOWN)
+            || solveWithDirection(map, flow, initialFlowList, initialFlowIndex, startX - 1, startY, LEFT)
+            || solveWithDirection(map, flow, initialFlowList, initialFlowIndex, startX + 1, startY, RIGHT);
     }
 
     private boolean solveWithDirection(GameMap map,
@@ -50,9 +62,7 @@ public class Solver {
         // If the end of the initial flow is reached
         // Change the initial flow
         if (initialFlow.p2().x() == x && initialFlow.p2().y() == y) {
-            System.out.println("The color's end is reached!!");
             if (initialFlowIndex == initialFlowList.size() - 1) {
-                System.out.println("Game is solved!!!");
                 return true;
             }
             final var nextInitialFlow = initialFlowList.get(initialFlowIndex + 1);
@@ -75,15 +85,6 @@ public class Solver {
                 matrix[y][x] = null;
             }
         }
-        return false;
-    }
-
-    private boolean isGameSolved(GameMap map) {
-        // TODO: To be implemented
-//        final var matrix = map.getMatrix();
-//        if (Arrays.stream(matrix).flatMap(Arrays::stream).anyMatch(Objects::isNull)) {
-//            return false;
-//        }
         return false;
     }
 
