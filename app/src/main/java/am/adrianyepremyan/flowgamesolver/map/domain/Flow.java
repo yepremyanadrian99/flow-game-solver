9667b4dbebd5cb7aa05e759e3eb19b84e7cce414
Yepremyan <adrian.yepremyan@picsart.com>
2022-11-24 14:20:11 +0400

    implemented working algorithm, the game is now being solved.

--- app/src/main/java/am/adrianyepremyan/flowgamesolver/map/domain/Flow.java
+++ app/src/main/java/am/adrianyepremyan/flowgamesolver/map/domain/Flow.java
@@ -4,10 +4,12 @@ import am.adrianyepremyan.flowgamesolver.helper.Point;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @AllArgsConstructor
 @Getter
 @Setter
+@ToString
 public class Flow {
 
     private Point point;
--- app/src/main/java/am/adrianyepremyan/flowgamesolver/solver/Solver.java
+++ app/src/main/java/am/adrianyepremyan/flowgamesolver/solver/Solver.java
@@ -4,8 +4,10 @@ import am.adrianyepremyan.flowgamesolver.helper.Point;
 import am.adrianyepremyan.flowgamesolver.map.GameMap;
 import am.adrianyepremyan.flowgamesolver.map.domain.Flow;
 import am.adrianyepremyan.flowgamesolver.map.domain.FlowDirection;
+import am.adrianyepremyan.flowgamesolver.map.domain.InitialFlow;
 import am.adrianyepremyan.flowgamesolver.map.printer.DefaultGameMapPrinter;
 import am.adrianyepremyan.flowgamesolver.map.printer.GameMapPrinter;
+import java.util.List;
 
 public class Solver {
 
@@ -15,29 +17,50 @@ public class Solver {
         final var matrix = map.getMatrix();
         final var initialFlow = map.getInitialFlowList().get(0);
 
-        if (!solveRecursively(map, matrix[initialFlow.p1().y()][initialFlow.p1().x()])) {
+        if (!solveRecursively(map, matrix[initialFlow.p1().y()][initialFlow.p1().x()], map.getInitialFlowList(), 0)) {
             throw new RuntimeException("Game has no solution!");
         }
     }
 
-    private boolean solveRecursively(GameMap map, Flow flow) {
-        System.out.println("Solving recursively for...");
+    private boolean solveRecursively(GameMap map, Flow flow,
+                                     List<InitialFlow> initialFlowList,
+                                     int initialFlowIndex) {
+        System.out.println("Solving recursively for flow: " + flow);
         printer.print(map);
-        if (isGameSolved(map)) {
-            return true;
-        }
 
         final int startX = flow.getPoint().x();
         final int startY = flow.getPoint().y();
 
-        return solveWithDirection(map, flow, startX, startY - 1, FlowDirection.UP)
-            || solveWithDirection(map, flow, startX, startY + 1, FlowDirection.DOWN)
-            || solveWithDirection(map, flow, startX - 1, startY, FlowDirection.LEFT)
-            || solveWithDirection(map, flow, startX + 1, startY, FlowDirection.RIGHT);
+        return solveWithDirection(map, flow, initialFlowList, initialFlowIndex, startX, startY - 1, FlowDirection.UP)
+            || solveWithDirection(map, flow, initialFlowList, initialFlowIndex, startX, startY + 1, FlowDirection.DOWN)
+            || solveWithDirection(map, flow, initialFlowList, initialFlowIndex, startX - 1, startY, FlowDirection.LEFT)
+            ||
+            solveWithDirection(map, flow, initialFlowList, initialFlowIndex, startX + 1, startY, FlowDirection.RIGHT);
     }
 
-    private boolean solveWithDirection(GameMap map, Flow currentFlow, int x, int y, FlowDirection directionToGo) {
+    private boolean solveWithDirection(GameMap map,
+                                       Flow currentFlow,
+                                       List<InitialFlow> initialFlowList,
+                                       int initialFlowIndex,
+                                       int x, int y,
+                                       FlowDirection directionToGo) {
         final var matrix = map.getMatrix();
+        final var initialFlow = initialFlowList.get(initialFlowIndex);
+
+        // If the end of the initial flow is reached
+        // Change the initial flow
+        if (initialFlow.p2().x() == x && initialFlow.p2().y() == y) {
+            System.out.println("The color's end is reached!!");
+            if (initialFlowIndex == initialFlowList.size() - 1) {
+                System.out.println("Game is solved!!!");
+                return true;
+            }
+            final var nextInitialFlow = initialFlowList.get(initialFlowIndex + 1);
+            final var nextFlow = matrix[nextInitialFlow.p1().y()][nextInitialFlow.p1().x()];
+
+            return solveRecursively(map, nextFlow, initialFlowList, initialFlowIndex + 1);
+        }
+
         if ((currentFlow.getDirection() == null || directionToGo != currentFlow.getDirection().getOpposite())
             && y >= 0 && y < matrix.length
             && x >= 0 && x < matrix[y].length) {
@@ -45,7 +68,7 @@ public class Solver {
             final var insertedFlow = insertFlowWithDirection(matrix, x, y, currentFlow.getColor(), directionToGo);
             if (insertedFlow != null) {
                 // Solve recursively with the new flow
-                if (solveRecursively(map, insertedFlow)) {
+                if (solveRecursively(map, insertedFlow, initialFlowList, initialFlowIndex)) {
                     return true;
                 }
                 // Revert the step
